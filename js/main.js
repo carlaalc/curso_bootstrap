6f86f84b22c8d25133cee82ec3b7ace2a461a506
carlaalc <45856524+carlaalc@users.noreply.github.com>
2019-02-12 20:07:57 -0200

    Funcionalidade de cadastro

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*o modal aparece e desaparece*/
+setTimeout(function(){
+	$("#cadastro_modal").modal("show");
+}, 1000);
+setTimeout(function(){
+	$("#cadastro_modal").modal("hide");
+}, 10000);
+/*o modal terá um formulário*/
