c2b057d50934d88e251e60e063a85db3ef22f54d
carlaalc <45856524+carlaalc@users.noreply.github.com>
2019-02-14 20:08:20 -0200

    Inclusão da sessão vendas

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ setTimeout(function(){
 	$("#cadastro_modal").modal("hide");
 }, 10000);
 /*o modal terá um formulário*/
+/*troca ler mais para ler menos*/
+$('#vendas_collapse').on('show.bs.collapse', function () {
+  $('a[href="#vendas_collapse"]').text("Ver menos")
+})
+$('#vendas_collapse').on('hidden.bs.collapse', function () {
+  $('a[href="#vendas_collapse"]').text("Ver mais")
+})
