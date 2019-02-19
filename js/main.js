20da83a6c04917ce5e375837276520a3a4bbf5bd
carlaalc <45856524+carlaalc@users.noreply.github.com>
2019-02-19 18:39:24 -0300

    Final da Aula 5 - Copiei todo o código do Gabriel

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-/*o modal aparece e desaparece*/
-setTimeout(function(){
-	$("#cadastro_modal").modal("show");
-}, 1000);
-setTimeout(function(){
-	$("#cadastro_modal").modal("hide");
-}, 10000);
-/*o modal terá um formulário*/
-/*troca ler mais para ler menos*/
+// setTimeout(function(){
+// 	$("#cadastro_modal").modal("show");
+// }, 100)
+
+// setTimeout(function(){
+// 	$("#cadastro_modal").modal("hide");
+// }, 5000)
+
 $('#vendas_collapse').on('show.bs.collapse', function () {
   $('a[href="#vendas_collapse"]').text("Ver menos")
 })
+
 $('#vendas_collapse').on('hidden.bs.collapse', function () {
   $('a[href="#vendas_collapse"]').text("Ver mais")
 })
