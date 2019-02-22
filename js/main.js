52df88c44ba6862af84a42a28965934a19501af8
carlaalc <45856524+carlaalc@users.noreply.github.com>
2019-02-21 21:12:52 -0300

    header fixo com sombra dinâmica

--- js/main.js
+++ js/main.js
@@ -13,3 +13,13 @@ $('#vendas_collapse').on('show.bs.collapse', function () {
 $('#vendas_collapse').on('hidden.bs.collapse', function () {
   $('a[href="#vendas_collapse"]').text("Ver mais")
 })
+
+$(window).on("scroll", function(){
+	var distanciaDoTopo = $(window).scrollTop();
+
+	if (distanciaDoTopo > 400) {
+		(distanciaDoTopo > 400;
+	} 
+	$("header_shadow").css("top", (distanciaDoTopo / 15) + "px");
+	}
+})
